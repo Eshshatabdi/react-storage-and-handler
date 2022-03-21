6683dc2d00de250b9b4c9dd9eb57f6f9aa9f3fa9
Esh shatabdi <eshshatabdi@gmail.com>
2022-03-20 22:35:39 -0400

    local handler cart

--- src/Utilities/Fakedb.js
+++ src/Utilities/Fakedb.js
@@ -1,5 +1,6 @@
 const addCart = id => {
     let shoppingCart;
+    // get the shopping cart ekhane parse int kora hoyeche jate shopping cart gula k jate object akare pawa jay.....{'','' ,}
     const storedCart = localStorage.getItem('shopping-cart')
     if (storedCart) {
         shoppingCart = JSON.parse(storedCart)
@@ -7,6 +8,8 @@ const addCart = id => {
     else {
         shoppingCart = {};
     }
+
+    // add quantity shopping cat k [id] neya hoyeche jate key te shopping-cart e dekhay ar parseint korahoyeche jate jotogula bare ta sonkhay ase
     const quantity = shoppingCart[id];
     if (quantity) {
         const newQuantity = parseInt(quantity) + 1
@@ -20,6 +23,7 @@ const addCart = id => {
         // localStorage.setItem(id, 1)
 
     }
+    // ekane stringify kora hoyeche karon value gula sring e thake tai prothome etake string e korte hoy
     localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
 
 
